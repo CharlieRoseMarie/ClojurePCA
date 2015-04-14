e94ba506aa9bf41897b5c47e722450f53ab74012
TAllen <takev.takev@gmail.com>
2015-04-13 21:36:34 -0700

    Removed unneeded functions.

--- src/pca/core.clj
+++ src/pca/core.clj
@@ -10,16 +10,30 @@
 (defn import-matrix [filename]
   (to-matrix (read-dataset filename :delim \tab)))
 
+(defn check-threshold [values thresh]
+  ; Returns false if the difference between the new and old values
+  ; is less than the threshold
+  (let [[old new] values]
+    (> (abs (- (sum-of-squares old)
+               (sum-of-squares new)))
+       (* (sum-of-squares new) thresh))))
+
 (defn normalize [v]
   (let [sum-of (sum-of-squares v)]
     (when-not (zero? sum-of)
       (div v (Math/sqrt sum-of)))))
 
-(defn PCA [m]
-  (->> m
-       covariance
-       decomp-eigenvalue
-       sort-components))
+(defn get-nth-col [m n]
+  (sel m :cols n))
+
+(defn matrix-projection [m v]
+  (when-not (zero? (sum-of-squares v))
+    (let [projection (mmult m v)]
+      (div projection (sum-of-squares v)))))
+
+(defn auto-projection [m]
+  (let [v (get-nth-col m 0)]
+    (matrix-projection (trans m) v)))
 
 (defn sort-components [eigens]
   (let [{:keys [values vectors]} eigens
@@ -27,11 +41,11 @@
         sorted-components (sort-by first > coupled-values)]
     (map second sorted-components)))
 
-(defn calc-component-phase [x u]
-  (->> u
-       (matrix-projection x)
-       normalize
-       (matrix-projection (trans x))))
+(defn PCA [m]
+  (->> m
+       covariance
+       decomp-eigenvalue
+       sort-components))
 
 (defn calc-scales-and-loadings-pass [m t]
   (let [p (normalize (matrix-projection (trans m) t))]
@@ -53,26 +67,6 @@
         (iterate (fn [[sl x]]
                    [(calc-scales-and-loadings x) (minus x (correction-matrix sl))])
                    [(calc-scales-and-loadings m) (minus m (correction-matrix (calc-scales-and-loadings m)))]))))
-
-(defn check-threshold [values thresh]
-  ; Returns false if the difference between the new and old values
-  ; is less than the threshold
-  (let [[old new] values]
-    (> (abs (- (sum-of-squares old)
-               (sum-of-squares new)))
-       (* (sum-of-squares new) thresh))))
-
-(defn matrix-projection [m v]
-  (when-not (zero? (sum-of-squares v))
-    (let [projection (mmult m v)]
-      (div projection (sum-of-squares v)))))
-
-(defn auto-projection [m]
-  (let [v (get-nth-col m 0)]
-    (matrix-projection (trans m) v)))
-
-(defn get-nth-col [m n]
-  (sel m :cols n))
 
 (defn matrix-mean [m]
   (map mean m))
